1cdb2ea58a3af4bf21754e87304fe7a024a3c2fa
kannapatton <kandirbrts@gmail.com>
2022-10-07 13:40:07 -0500

    not pretty or perfect, but done

--- server/index.js
+++ server/index.js
@@ -48,7 +48,70 @@ app.get('/menu', (req, res) =>{
         }
     });
 });
+// app.put('/update', (req, res) =>{
+//     const id = req.body.id 
+//     const wage = req.body.wage;
+//     db.query("UPDATE dinner SET monday = ? WHERE id = ?",
+//     [monday, id],
+//     (err, result) =>{
+//         if (err) {
+//             console.log(err)
+//         } else{
+//             res.send(result);
+//         }
+//     });
+// });
+app.post('/signup', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    db.query('INSERT INTO users (username, password) VALUES (?,?)', 
+    [username, password], 
+    // callback function that represents what will happen after above statement is done
+    (err, result) =>{
+        if (err) {
+            console.log(err)
+        } else {
+            res.send('Values Inserted');
+        }
+    }
+    );
 
+})
+app.get('/login', (req, res) =>{
+    const { username, password } = req.body;
+  
+    let sql = "SELECT * FROM users WHERE username = ?";
+    sql = mysql.format(sql, [username]);
+  
+    pool.query(sql, (err, results) => {
+      if (err) return handleSQLError(res, err);
+      if (!results.length) return res.status(404).send("No matching users");
+  
+      const hash = results[0].password;
+      bcrypt.compare(password, hash).then((result) => {
+        if (!result) return res.status(400).send("Invalid password");
+  
+        const data = { ...results[0] };
+        data.password = "REDACTED";
+  
+        const token = jwt.sign(data, process.env.JWT_SECRET, {expiresIn: "3h"})
+                  res.json({
+                      msg: "Logged in " + username,
+                      token
+        });
+      });
+    });
+});
+app.delete('/delete/:id', (req, res) => {
+    const iddinner = req.params.id;
+    db.query("DELETE FROM dinner WHERE iddinner = ?", iddinner, (err, result) => {
+        if (err) {
+            console.log (err);
+        } else {
+            res.send(result);
+        }
+    });
+    });
 
 
 
